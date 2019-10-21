fe0caa8b8122917b5461caf149503a0bb2b63e38
brad hemmerick <brad.hemmerick13@gmail.com>
2019-10-21 19:07:55 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,15 +4,17 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
- minus = num1 -num2
+ minus = num2 - num1
+ p minus
 end
 
 def division(num1, num2)
-
+  divide = num1/num2
+  p divide
 end
 
 def multiplication(num1, num2)
-
+  multi = num1 * num2
 end
 
 def modulo(num1, num2)
